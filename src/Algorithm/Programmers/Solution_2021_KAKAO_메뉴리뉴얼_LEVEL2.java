9423c2cc6c7222d612d6ee5b8989407153328ad1
jinho_pca <jinho_pca@naver.com>
2021-05-07 00:02:41 +0900

    [Create] Solution_2021_KAKAO_메뉴리뉴얼_LEVEL2.java

---
+++ src/Algorithm/Programmers/Solution_2021_KAKAO_메뉴리뉴얼_LEVEL2.java
@@ -0,0 +1,108 @@
+package Algorithm.Programmers;
+
+import java.util.*;
+
+public class Solution_2021_KAKAO_메뉴리뉴얼_LEVEL2 {
+    List<Map<String, Integer>> map = new ArrayList<>();
+    int[] maxCnt = new int[11];
+
+    public String[] solution(String[] orders, int[] course) {
+        for(int i = 0; i < 11; i++){
+            map.add(new HashMap<String, Integer>());
+        }
+
+        // 알파벳 순 정렬
+        for(String str : orders){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            comb(arr, 0, new StringBuilder());
+        }
+
+        List<String> list = new ArrayList<>();
+        for(int len : course){
+            for(Map.Entry<String, Integer> entry : map.get(len).entrySet()){
+                if(entry.getValue() >= 2 && entry.getValue() == maxCnt[len]){
+                    list.add(entry.getKey());
+                }
+            }
+        }
+
+        // 오름차순 성렬
+        Collections.sort(list);
+
+        String[] answer = new String[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    public void comb(char[] str, int pos, StringBuilder sb){
+        if(pos >= str.length){
+            int len = sb.length();
+            if(len >= 2){
+                int cnt = map.get(len).getOrDefault(sb.toString(), 0) + 1;
+                map.get(len).put(sb.toString(), cnt);
+                maxCnt[len] = Math.max(maxCnt[len], cnt);
+            }
+            return;
+        }
+        comb(str, pos+1, sb.append(str[pos])); // 선택 O
+        sb.setLength(sb.length()-1); // append 복원
+        comb(str, pos+1, sb); // 선택 X
+    }
+} // end of class
+
+/*
+import java.util.*;
+class Solution {
+    List<Map<String, Integer>> map = new ArrayList<>();
+    int[] maxCnt = new int[11];
+
+    public String[] solution(String[] orders, int[] course) {
+        for(int i = 0; i < 11; i++){
+            map.add(new HashMap<String, Integer>());
+        }
+
+        // 알파벳 순 정렬
+        for(String str : orders){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            comb(arr, 0, new StringBuilder());
+        }
+
+        List<String> list = new ArrayList<>();
+        for(int len : course){
+            for(Map.Entry<String, Integer> entry : map.get(len).entrySet()){
+                if(entry.getValue() >= 2 && entry.getValue() == maxCnt[len]){
+                    list.add(entry.getKey());
+                }
+            }
+        }
+
+        // 오름차순 성렬
+        Collections.sort(list);
+
+        String[] answer = new String[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    public void comb(char[] str, int pos, StringBuilder sb){
+        if(pos >= str.length){
+            int len = sb.length();
+            if(len >= 2){
+                int cnt = map.get(len).getOrDefault(sb.toString(), 0) + 1;
+                map.get(len).put(sb.toString(), cnt);
+                maxCnt[len] = Math.max(maxCnt[len], cnt);
+            }
+            return;
+        }
+        comb(str, pos+1, sb.append(str[pos])); // 선택 O
+        sb.setLength(sb.length()-1); // append 복원
+        comb(str, pos+1, sb); // 선택 X
+    }
+}
+ */
