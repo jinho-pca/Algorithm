be96592304f3359f46c54802749f9e0b7c876edc
jinho_pca <jinho_pca@naver.com>
2021-12-19 14:15:56 +0900

    [Create] Solution_내적.java

---
+++ src/Algorithm/Programmers/LEVEL1/Solution_내적.java
@@ -0,0 +1,11 @@
+package Algorithm.Programmers.LEVEL1;
+
+public class Solution_내적 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
