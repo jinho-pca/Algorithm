bf2664a286af03890ecd430b0150aed3df05ea2d
jinho_pca <jinho_pca@naver.com>
2022-02-23 15:53:13 +0900

    [Update] Main_BOJ_1504_특정한최단경로_골드4_이진호.java

--- src/Algorithm/BOJ/Main_BOJ_1504_특정한최단경로_골드4_이진호.java
+++ src/Algorithm/BOJ/Main_BOJ_1504_특정한최단경로_골드4_이진호.java
@@ -9,10 +9,20 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main_BOJ_1504_특정한최단경로_골드4_이진호 {
-    static final int INF = 1000000;
+    static final long INF = 1000000000;
+    static int N, E, v1, v2;
+    static long result;
     static ArrayList<Node>[] graph;
-    static int N, E, from, to, cost, v1, v2, result;
+    static boolean[] visited;
+    static class Node {
+        int to;
+        long cost;
 
+        public Node(int to, long value) {
+            this.to = to;
+            this.cost = value;
+        }
+    }
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -26,9 +36,9 @@ public class Main_BOJ_1504_특정한최단경로_골드4_이진호 {
 
         for(int i = 1; i <= E; i++){
             st = new StringTokenizer(br.readLine());
-            from = Integer.parseInt(st.nextToken());
-            to = Integer.parseInt(st.nextToken());
-            cost = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            long cost = Long.parseLong(st.nextToken());
 
             graph[from].add(new Node(to, cost));
             graph[to].add(new Node(from, cost));
@@ -37,22 +47,27 @@ public class Main_BOJ_1504_특정한최단경로_골드4_이진호 {
         v1 = Integer.parseInt(st.nextToken());
         v2 = Integer.parseInt(st.nextToken());
 
-        result = dijkstra(1, v1) + dijkstra(v2, N) < dijkstra(1, v2) + dijkstra(v1, N) ? dijkstra(1, v1) + dijkstra(v2, N) : dijkstra(1, v2) + dijkstra(v1, N);
-        result += dijkstra(v1, v2);
+        long dist1tov1 = dijkstra(1, v1);
+        long dist1tov2 = dijkstra(1, v2);
+        long distv2toN = dijkstra(v2, N);
+        long distv1toN = dijkstra(v1, N);
+        long distv1tov2 = dijkstra(v1, v2);
+        result = dist1tov1 + distv2toN < dist1tov2 + distv1toN ? dist1tov1 + distv2toN : dist1tov2 + distv1toN;
+        result += distv1tov2;
 
         System.out.print(result >= INF ? -1 : result);
     } // end of main
-    static int dijkstra(int start, int end){
+    static long dijkstra(int start, int end){
         if(start == end) return 0;
-        int[] dist = new int[N +1];
+        long[] dist = new long[N +1];
         Arrays.fill(dist, INF);
         dist[start] = 0;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {return Long.compare(o1.cost, o2.cost);});
         pq.add(new Node(start, 0));
 
         while(!pq.isEmpty()){
             Node current = pq.poll();
-            if(current.to== end) return dist[current.to];
+            if(current.to == end) return dist[current.to];
             for(int i = 0; i < graph[current.to].size(); i++){
                 Node next = graph[current.to].get(i);
                 if(dist[next.to] > dist[current.to] + next.cost){
@@ -63,18 +78,4 @@ public class Main_BOJ_1504_특정한최단경로_골드4_이진호 {
         }
         return INF;
     } // end of dijkstra
-    static class Node implements Comparable<Node>{
-        int to;
-        int cost;
-
-        public Node(int to, int value) {
-            this.to = to;
-            this.cost = value;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.cost >o.cost ? 1:-1;
-        }
-    }
 } // end of class
